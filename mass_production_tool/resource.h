803037c7344e4655460f7f30c18c906d527d2f54
mrwiz77 <38714327+mrwiz77@users.noreply.github.com>
2026-06-15 14:25:26 +0900

    기능 4차 버전

--- mass_production_tool/resource.h
+++ mass_production_tool/resource.h
@@ -36,8 +36,12 @@
 #define IDC_DEC_NORMAL                  1017
 #define IDC_MAIN_RDO_CAN                1018
 #define IDC_MAIN_RDO_UART               1019
+#define IDC_RNG_LOOP                    1019
+#define IDC_VALUE_LOOP                  1019
 #define IDC_RADIO3                      1020
 #define IDC_MAIN_RDO_ETH                1020
+#define IDC_EDIT1                       1020
+#define IDC_SYS_LOOP                    1020
 #define IDC_DEC_NORMAL2                 1021
 #define IDC_DEC_NETWORK                 1021
 #define IDC_DEC_SYS_LOG                 1022
@@ -49,7 +53,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
